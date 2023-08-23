96846b09d83fc5a305db1cafeb213b20e1c8dff9
Gcruz10 <gabrielcruz2335@gmail.com>
2023-08-23 19:41:17 -0400

    fixed signup and added to login

--- clientt/src/pages/Signup/Signup.jsx
+++ clientt/src/pages/Signup/Signup.jsx
@@ -3,76 +3,83 @@ import { useState } from 'react';
 import {Link} from "react-router-dom";
 import "./Signup.scss"
 import Auth from "../utils/auth";
+import { useMutation } from '@apollo/client';
 
 function Signup() {
-    const [formState, setFormState] = useState({ email: '', password: '', username: '' });
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
     const [addUser, { error }] = useMutation(ADD_USER);
-
+  
     const handleFormSubmit = async (event) => {
         event.preventDefault();
-        const mutationResponse = await addUser({
-            variables: {
-                email: formState.email, 
-                password: formState.password
-                username: formState.username
-            }
-        });
-        const token = mutationResponse.data.addUser.token;
-        Auth.login(token);
+        try {
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
     };
-}
-
-return (
-    <div className="container my-1">
-        <Link to="/login">
-            ← Go to Login
-        </Link>
-
-        <h2>Signup</h2>
-        <form onSubmit={handleFormSubmit}>
-            <div className="flex-row space-between my-2">
-                <label htmlFor="username">Username:</label>
-                <input
-                    placeholder="Username"
-                    name="username"
-                    type="text"
-                    value={formState.username}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="flex-row space-between my-2">
-                <label htmlFor="email">Email:</label>
-                <input
-                    placeholder="Email"
-                    name="email"
-                    type="email"
-                    value={formState.email}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className="flex-row space-between my-2">
-                <label htmlFor="pwd">Password:</label>
-                <input
-                    placeholder="Password"
-                    name="password"
-                    type="password"
-                    value={formState.password}
-                    onChange={handleChange}
-                />
+  
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+  
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+  
+    return (
+        <div className="signup">
+            <div className="wrapper">
+                <div className="title">
+                    <h1>Sign Up</h1>
                 </div>
-                {error ? (
-                    <div>
-                        <p className="error-text">The provided credentials are incorrect</p>
-                    </div>
-                ) : null}
-            <div className="flex-row flex-end">
-                <button type="submit">
-                    Submit
-                </button>
+                <div className="form">
+                    <form onSubmit={handleFormSubmit}>
+                        <div className="username">
+                            <label htmlFor="username">Username</label>
+                            <input
+                                placeholder="Your username"
+                                name="username"
+                                type="username"
+                                id="username"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="email">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                placeholder="Your email"
+                                name="email"
+                                type="email"
+                                id="email"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="password">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                placeholder="Your password"
+                                name="password"
+                                type="password"
+                                id="password"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="submit">
+                            <button type="submit">Sign Up</button>
+                        </div>
+                        <div className="link">
+                            <Link to="/login">Already have an account? Login</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
-    </div>
-);
+        </div>
+    )
+}
 
 export default Signup;
 
