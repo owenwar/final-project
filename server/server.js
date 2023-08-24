3de2e74fbbdcfbf8ce53169e4374115dc64403db
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-08-23 22:23:15 -0400

    10:22

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path'); 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const auth = require('./middleware/auth');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,10 +17,14 @@ require('./config/db.js');
 
 // Routes
 app.use('/', require('./routes'));
-app.use(express.static('./client/build'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
