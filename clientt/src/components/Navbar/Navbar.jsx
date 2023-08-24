e960c975221f19ad84f15f0eed923f142e2a8558
owenwar <121326370+owenwar@users.noreply.github.com>
2023-08-23 20:10:32 -0400

    .

--- clientt/src/components/Navbar/Navbar.jsx
+++ clientt/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import {Link} from "react-router-dom";
+import Cart from "../Cart/Cart";
 import "./Navbar.scss"
 
 const Navbar = () => {
-  const [open, setOpen]
+  const [open, setOpen] = useState(false)
   return (
     <div className='navbar'>
       <div className="wrapper"> 
@@ -23,7 +24,7 @@ const Navbar = () => {
         </div>
 
         <div className="item">
-          <Link className='link' to="/products/3">Womenswear</Link>
+          <Link className='link' to="/products/3">Accessories</Link>
         </div>
 
         </div>
@@ -33,29 +34,18 @@ const Navbar = () => {
 
         <div className="right">
 
-        <div className="item">
-          <Link className='link' to="/">About</Link>
-        </div>
-
-        <div className="item">
-          <Link className='link' to="/">Contact</Link>
-        </div>
-
-        <div className="item">
-          <Link className='link' to="/">Stores</Link>
-        </div>
-
         <div className="icons">
           <SearchOutlinedIcon/>
           <PersonOutlineOutlinedIcon/>
           <FavoriteBorderOutlinedIcon/>
-          <div className="cartIcon">
+          <div className="cartIcon" onClick={()=>setOpen(!open)}>
             <ShoppingCartOutlinedIcon/>
             <span>0</span>
           </div>
         </div>
         </div>
       </div>
+      {open && <Cart/>}
     </div>
   )
 }
