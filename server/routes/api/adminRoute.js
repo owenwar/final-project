0f67a3f9f46d9c6d2c58e59402eb1c8bdd8f2e40
Dominick-D <127568682+Dominick-D@users.noreply.github.com>
2023-08-26 03:19:21 -0400

    8/26 3:14AM
    Added admin page,
    Didnt add styling to the page yet,
    Hooked up AWS S3 to handle photos (adminRoute.js is just for the AWS S3,

---
+++ server/routes/api/adminRoute.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const AWS = require('aws-sdk');
+const multer = require('multer');
+
+const router = express.Router();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+});
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // no larger than 5mb
+  }
+});
+
+router.post('/upload', upload.single('image'), (req, res) => {
+  const file = req.file;
+  const s3FileURL = process.env.AWS_UPLOADED_FILE_URL_LINK;
+
+  let s3bucket = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+  });
+
+  var params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: file.originalname,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+    ACL: "public-read"
+  };
+
+  s3bucket.upload(params, function(err, data) {
+    if (err) {
+      res.status(500).json({ error: true, Message: err });
+    } else {
+      res.send({ data });
+    }
+  });
+});
+
+module.exports = router;
